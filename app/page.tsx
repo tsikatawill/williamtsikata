6946a7cf55b05f95788a86ba3e07d8ce1cfacadf
William M. Tsikata <tsikatawill@gmail.com>
2023-08-08 04:51:58 +0000

    fix caching issue

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import Navbar from "@/components/Navbar";
 import Projects from "@/components/Projects";
 import Stack from "@/components/Stack";
 
+export const revalidate = 60;
+
 export default function Home() {
   return (
     <>
