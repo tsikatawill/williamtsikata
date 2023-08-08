da2d650b52f28104d282a0efbea93e35034501dd
William M. Tsikata <tsikatawill@gmail.com>
2023-08-08 04:47:43 +0000

    make icon white

--- components/SocialPill.tsx
+++ components/SocialPill.tsx
@@ -12,7 +12,7 @@ const SocialPill = ({ socialLink }: { socialLink: SocialLink }) => {
       rel="noreferrer"
       className=" w-20 h-20 p-1 sm:w-28 sm:h-28 flex items-center justify-center flex-col gap-3 bg-primary hover:bg-primaryLight rounded-md text-center"
     >
-      <div className="w-10 text-white">
+      <div className="w-10 filter invert">
         <Image
           src={urlForImage(image).url()}
           width={300}
